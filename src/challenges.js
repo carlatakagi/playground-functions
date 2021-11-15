3ee351be9ceec0ae0ab01c5349aeba694e4b6183
carlatakagi <carlatakagi@gmail.com>
2021-11-15 17:19:31 -0400

    desafios 1 a 7

--- src/challenges.js
+++ src/challenges.js
@@ -2,12 +2,13 @@
 
 // Desafio 1
 function compareTrue(valor1, valor2) {
-  
-  if (valor1 === true && valor2 === true){
-    return true;
+  let resultado;
+  if (valor1 === true && valor2 === true) {
+    resultado = true;
   } else {
-    return false;
+    resultado = false;
   }
+  return resultado;
 }
 console.log(compareTrue(true, true)); // ok
 
@@ -16,7 +17,6 @@ function calcArea(base, height) {
   let area = (base * height) / 2;
 
   return area;
-
 }
 console.log(calcArea(10, 50));
 console.log(calcArea(5, 2));
@@ -25,52 +25,55 @@ console.log(calcArea(51, 1)); // ok
 // Desafio 3
 // consultei o método split no DevMedia
 // Link: https://www.devmedia.com.br/javascript-split-dividindo-separando-strings/39254
-function splitSentence() {
-  let sentence = 'go Trybe';
-  
-  return sentence.split(" ");
-
+function splitSentence(string) {
+  return string.split(' ');
 }
-console.log(splitSentence()); //ok
-
+console.log(splitSentence('go trybe')); // ok
 
 // Desafio 4
-function concatName() {
-  // ao receber uma ARRAY DE STRINGS
-  //retorna uma string com o formato 'ULTIMO ITEM, PRIMEIRO ITEM'
-  let arrNames = ['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'];
-  // [0, 1, 2, 3]
-  for (let i = 0; i < arrNames.length; i++) {
+// consultei como pegar itens no StackOverflow
+// Link: https://stackoverflow.com/questions/43714111/get-the-first-and-last-item-in-an-array-js/43714143
+function concatName(arrNames) {
+  let firstElement = arrNames[0];
+  let lastElement = arrNames[arrNames.length - 1];
+  let lastAndFirst = lastElement + ', ' + firstElement;
 
-  }
-
-
+  return lastAndFirst;
 }
+console.log(concatName(['Lucas', 'Cassiano', 'Ferraz', 'Paolillo'])); // ok
 
 // Desafio 5
 function footballPoints(wins, ties) {
-  //para cada vitoria soma 3 pontos - wins * 3
+  // para cada vitoria soma 3 pontos - wins * 3
   // para cada empate soma 1 ponto - ties * 1
   let winsPoints = wins * 3;
   let tiesPoints = ties;
   let totalPoints = winsPoints + tiesPoints;
 
-  return totalPoints
+  return totalPoints;
 }
 
 console.log(footballPoints(14, 8));
 console.log(footballPoints(1, 2));
-console.log(footballPoints(0, 0)); //ok
+console.log(footballPoints(0, 0)); // ok
 
 // Desafio 6
 function highestCount() {
   // seu código aqui
+  // ainda nao
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(cat1, cat2, mouse) {
+  if (cat1 === mouse - 3 && cat2 === mouse - 2) {
+    return 'cat2';
+  } else if (cat1 === mouse - 6 && cat2 === mouse - 12) {
+    return 'cat1';
+  } else if (cat1 === cat2) {
+    return 'os gatos trombam e o rato foge';
+  }
 }
+console.log(catAndMouse(44, 38, 50));
 
 // Desafio 8
 function fizzBuzz() {
